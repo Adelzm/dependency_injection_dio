1410be3c0122658101e82d1d7b32f97f7b8ecb24
Adel Muhandes <adel.muhandes@liebherr.com>
2024-03-24 14:56:28 +0100

    Add HomeScreen to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:dependency_injection_dio/presentation/screens/home_screen.dart';
+import 'package:dependency_injection_dio/services/injector.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
   runApp(const MainApp());
 }
 
@@ -10,11 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
